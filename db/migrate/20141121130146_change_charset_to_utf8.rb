65072596bb45328dec398d0c5ac1b44e8f884232
Piet Brauer <piet@nerdishbynature.com>
2015-04-17 09:12:04 +0200

    Don't change encoding for demo

--- db/migrate/20141121130146_change_charset_to_utf8.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCharsetToUtf8 < ActiveRecord::Migration
- 
-  def change
-    db_name = ActiveRecord::Base.connection.current_database
-    execute <<-SQL
-      ALTER DATABASE #{db_name} CHARACTER SET utf8 COLLATE utf8_general_ci;
-    SQL
-  end
-end
